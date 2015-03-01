ffc69c6cbd159e7c241fe70572543b9a4a6201d1
Vladimir <complover116@gmail.com>
2015-03-01 15:01:56 +0300

    Java version detection! YAY!

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <stdlib.h>
-
+#include <windows.h>
 using namespace std;
 
 const char version[] = "v0.1";
 
 using namespace std;
+
+LONG GetStringRegKey(HKEY hKey, const std::wstring &strValueName, std::wstring &strValue, const std::wstring &strDefaultValue)
+{
+    strValue = strDefaultValue;
+    WCHAR szBuffer[512];
+    DWORD dwBufferSize = sizeof(szBuffer);
+    ULONG nError;
+    nError = RegQueryValueExW(hKey, strValueName.c_str(), 0, NULL, (LPBYTE)szBuffer, &dwBufferSize);
+    if (ERROR_SUCCESS == nError)
+    {
+        strValue = szBuffer;
+    }
+    return nError;
+}
+std::wstring s2ws(const std::string& str)
+{
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo( size_needed, 0 );
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
 int main()
 {
     cout << "Starting PI3 launcher version " << version << ".." << endl;
     cout << "Detecting if java is installed..." << endl;
-    char javahome[] = getenv("JAVA_HOME");
-    cout << "JAVA_HOME=" << javahome << endl;
+    wstring value;
+    HKEY key;
+    if (RegOpenKey(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\JavaSoft\\Java Runtime Environment\\"), &key) != ERROR_SUCCESS)
+    {
+        cout << "Unable to open registry key";
+    }
+    GetStringRegKey(key, s2ws("CurrentVersion"), value, s2ws("ERROR"));
+    wcout << value << endl;
+    RegCloseKey(key);
+
     return 0;
 }
