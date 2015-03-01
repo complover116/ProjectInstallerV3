4386f3708ad6721cbeec110d7597200614eadd6d
Vladimir <complover116@gmail.com>
2015-03-01 13:22:30 +0300

    The beginning

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+const char version[] = "v0.1";
+
+using namespace std;
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Starting PI3 launcher version " << version << ".." << endl;
+    cout << "Detecting if java is installed..." << endl;
+    char javahome[] = getenv("JAVA_HOME");
+    cout << "JAVA_HOME=" << javahome << endl;
     return 0;
 }
